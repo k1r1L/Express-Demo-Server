01e46254ee80efddb8a64ce46cbcd47dc7928c6f
kircata <kircata>
2019-02-04 21:40:38 +0200

    Deleted about route

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,6 @@ const restrictedPages = require('./auth');
 
 module.exports = app => {
     app.get('/', controllers.home.index);
-    app.get('/about', restrictedPages.hasRole('Admin'), controllers.home.about);
     app.get('/register', controllers.user.registerGet);
     app.post('/register', controllers.user.registerPost);
     app.post('/logout', controllers.user.logout);
